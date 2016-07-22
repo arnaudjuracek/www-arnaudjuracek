9babb742ffc4525e23e716925b95268f72c31481
RNO <arnaud.juracek@gmail.com>
2016-07-22 15:32:14 +0200

    cursor: tweak physics

--- src/libs/cursor.js
+++ src/libs/cursor.js
@@ -9,7 +9,11 @@ function Cursor(_id, _x, _y, texture){
 		sprite.anchor.y = 0.5;
 
 	let hitbox = new window.Matter.Bodies.circle(x, y, 5, {
-		friction: 0.1,
+		density: 1,
+		friction: 0,
+		frictionStatic: 0.5,
+		frictionAir: 0.03,
+		restitution: 1,
 		mass: 100,
 		isStatic: true
 	});
