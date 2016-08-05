a86c064ec7e3c38aa48d93c765586ad853763b6f
RNO <arnaud.juracek@gmail.com>
2016-08-05 20:39:11 +0200

    add <video> controls
    fix #10

---
+++ src/libs/videoController.js
@@ -0,0 +1,67 @@
+function videoController(videoSelector, contollersSelector){
+
+	let videos;
+
+	function init(){
+		videos = document.querySelectorAll(videoSelector);
+		for(let i=0, l=videos.length; i<l; i++){
+			let video = videos[i];
+			let controls = video.querySelector(contollersSelector);
+			let player = video.querySelector('video');
+
+			player.controls = false;
+			controls.style.display = '';
+
+			bindControls(player, controls);
+		}
+	}
+
+	init();
+
+
+
+	// -------------------------------------------------------------------------
+
+	function bindControls(player, controls){
+		let duration = player.duration;
+		let progress = controls.querySelector('#progress');
+		let loaded = controls.querySelector('#loaded');
+
+		controls.addEventListener('click', function(){
+			if(player.paused || player.ended){
+				player.play();
+				controls.classList.add('playing');
+			}else{
+				player.pause();
+				controls.classList.remove('playing');
+			}
+		});
+
+		player.addEventListener('ended', function(){
+			play.pause();
+			controls.classList.remove('playing');
+		});
+
+		player.addEventListener('timeupdate', function(){
+			let time = player.currentTime;
+			let buffered = player.buffered.end(0)
+
+			progress.style.width = time * (100/player.duration) + "%";
+			loaded.style.width = buffered * (100/player.duration) + "%";
+		}, false);
+	}
+
+
+	// -------------------------------------------------------------------------
+
+	function revalidate(){
+		videos = [];
+		init();
+	}
+
+	return {
+		revalidate
+	};
+}
+
+export default videoController;
