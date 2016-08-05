50643dff23b26464d357ce899f433bc8d6861245
RNO <arnaud.juracek@gmail.com>
2016-08-05 23:19:51 +0200

    build/template: add support for srcset
    `npm run build` now auto-resize images from `build/assets/projects/`

--- scripts/utils/createBuild.js
+++ scripts/utils/createBuild.js
@@ -5,6 +5,8 @@ const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const mu                = require('mu2');
 const createConfig      = require('./createConfig');
 const sh                = require('./Shell');
+const walk              = require('fs-walk');
+const resize            = require('./resize.js');
 
 function createBuild(options = {}) {
 
@@ -15,51 +17,99 @@ function createBuild(options = {}) {
   let hash, compiler, output;
 
   const buildMustache = () => {
-    return new Promise((resolve, reject) => {
-      const compilerData = {
-        hash: hash,
-        isProduction: true
-      };
-      for (const k in pagesConfig.pages) {
-        const page = pagesConfig.pages[k];
-        let filename = path.join(output,k);
-        fs.ensureDir(path.dirname(filename), function (err) {
-            const fileStream = fs.createWriteStream(filename);
-            const templateStream = mu.compileAndRender(page.template,
-            Object.assign({compiler: compilerData}, page.data));
-            templateStream.pipe(fileStream);
-            templateStream.on('error', (e) => reject(e));
-            fileStream.on('finish', () => resolve());
-        });
-      }
-    });
+	return new Promise((resolve, reject) => {
+	  const compilerData = {
+		hash: hash,
+		isProduction: true
+	  };
+	  for (const k in pagesConfig.pages) {
+		const page = pagesConfig.pages[k];
+		let filename = path.join(output,k);
+		fs.ensureDir(path.dirname(filename), function (err) {
+			const fileStream = fs.createWriteStream(filename);
+			const templateStream = mu.compileAndRender(page.template,
+			Object.assign({compiler: compilerData}, page.data));
+			templateStream.pipe(fileStream);
+			templateStream.on('error', (e) => reject(e));
+			fileStream.on('finish', () => resolve());
+		});
+	  }
+	});
+  }
+
+  const resizeImages = () => {
+	return new Promise((resolve, reject) => {
+		const imageDirectory = path.join(output, 'assets/projects/');
+		const sizes = {
+			large: 1600,
+			// medium: 1280,
+			// small: 640
+		}
+
+		walk.filesSync(imageDirectory, function(basedir, filename, stat, next){
+			if((/\.(gif|jp?g|tiff|png)$/i).test(filename)){
+				let file = path.join(basedir, filename);
+
+  				for(let dirname in sizes){
+					let output = path.join(basedir, dirname, filename);
+					let size = sizes[dirname];
+					resize({
+				      	input: file,
+				      	output: output,
+				      	width: size
+				    });
+				}
+
+				// low-fi image for lazyloading
+				resize({
+					input: file,
+					output: path.join(basedir, 'low', filename),
+					width: 44,
+					quality: 0.1,
+					deleteAfterResize: true
+				});
+  			}
+		});
+
+	});
   }
 
   const run = () => {
-    return new Promise((resolve, reject) => {
-      mu.root = (pagesConfig.options && pagesConfig.options.cwd)
-        ? pagesConfig.options.cwd : muDefaultRoot;
-      compiler = webpack(config.webpackConfig);
-      compiler.apply(new ProgressBarPlugin());
-      compiler.run((err, stats) => {
-        if (!err) {
-          hash = stats.hash;
-          output = stats.compilation.outputOptions.path;
-          sh.info('Start building mustache templates...');
-          buildMustache()
-            .then(() => {
-              resolve()
-              sh.success('Build completed\n');
-            })
-            .catch((e) => {
-              reject(e)
-              sh.error('Build failed\n');
-            });
-        } else {
-          reject(err);
-        }
-      });
-    });
+	return new Promise((resolve, reject) => {
+	  mu.root = (pagesConfig.options && pagesConfig.options.cwd)
+		? pagesConfig.options.cwd : muDefaultRoot;
+	  compiler = webpack(config.webpackConfig);
+	  compiler.apply(new ProgressBarPlugin());
+	  compiler.run((err, stats) => {
+		if (!err) {
+		  hash = stats.hash;
+		  output = stats.compilation.outputOptions.path;
+		  sh.info('Start building mustache templates...');
+		  buildMustache()
+		    .then(() => {
+		      resolve()
+		      sh.success('Build completed\n');
+		    })
+		    .catch((e) => {
+		      reject(e)
+		      sh.error('Build failed\n');
+		    });
+
+		  sh.info('Start resizing images in projects/...');
+		  resizeImages()
+			.then(() => {
+			  resolve()
+			  sh.success('Images successfully resized\n');
+			})
+			.catch((e) => {
+			  reject(e)
+			  sh.error('Failed to resize images\n');
+			});
+		} else {
+		  reject(err);
+		}
+	  });
+	});
   }
 
   return { run };
---
+++ scripts/utils/resize.js
@@ -0,0 +1,39 @@
+const async = require('async');
+const fs = require('fs-extra');
+const im = require('imagemagick');
+const maxworkers = require('os').cpus().length;
+const path = require('path');
+
+
+module.exports = resize;
+
+function resize(params) {
+	let queue = async.queue(resizeimg, maxworkers);
+
+	queue.push({
+		input: params.input,
+		output: params.output,
+		width: params.width,
+		height: params.height,
+		quality: params.quality || 0.8,
+		deleteAfterResize: params.deleteAfterResize
+	});
+}
+
+function resizeimg(params, cb){
+	fs.ensureDir(path.dirname(params.output), function(err){
+		if(err){
+			console.log(err);
+		}else{
+			im.resize({
+				srcPath: params.input,
+				dstPath: params.output,
+				width:   params.width,
+				quality: params.quality
+			}, function(err, stdout, stderr){
+				if(err) console.log(stderr);
+				else if(params.deleteAfterResize) fs.unlinkSync(params.input);
+			});
+		}
+	});
+}
