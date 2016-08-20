e62f3a7da2615f8fb4db93d10469c98aa72dc22e
RNO <arnaud.juracek@gmail.com>
2016-08-21 01:53:51 +0200

    Revert "add express-status-monitor"
    This reverts commit a5efab5ad83f7b57ec6fc343d0bd0355e6601bb5.

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-app.use(require('express-status-monitor')());
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
